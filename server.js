ae45db2d05ef5a5d4770aecfb29ebc0d2a860ded
AlFahadMunna <afmunna.afm.afm@gmail.com>
2026-06-01 20:55:30 +0600

    update room endpoint added

--- server.js
+++ server.js
@@ -205,6 +205,54 @@ async function run() {
       }
     });
 
+    // update room
+
+    app.patch("/rooms/:id", verifyToken, async (req, res) => {
+      try {
+        const id = req.params.id;
+
+        const body = req.body;
+
+        const room = await roomsCollection.findOne({
+          _id: new ObjectId(id),
+        });
+
+        if (!room) {
+          return res.status(404).json({
+            success: false,
+            message: "Room not found",
+          });
+        }
+
+        if (room.userId !== req.user.id) {
+          return res.status(403).json({
+            success: false,
+            message: "Forbidden access",
+          });
+        }
+
+        const result = await roomsCollection.updateOne(
+          {
+            _id: new ObjectId(id),
+          },
+          {
+            $set: body,
+          },
+        );
+
+        return res.status(200).json({
+          success: true,
+          message: "Room updated successfully",
+          data: result,
+        });
+      } catch (error) {
+        return res.status(500).json({
+          success: false,
+          message: "Internal Server Error",
+        });
+      }
+    });
+
     // await client.db("admin").command({ ping: 1 });
     console.log("Connected to MongoDB!");
   } finally {
